b54db966e955357119086c9bfc5daff773f204c2
syphan <phantiensy@unimarmores.com>
2019-10-15 15:09:13 +0700

    Merge branch 'master' of https://gitlab.velacorp.vn/cosalon.vn/backend-migration-pos365
    # Conflicts:
    #	src/main/java/com/jidian/cosalon/migration/pos365/retrofitservice/Pos365RetrofitService.java
    #	src/main/java/com/jidian/cosalon/migration/pos365/service/TaskService.java

--- src/main/java/com/jidian/cosalon/migration/pos365/retrofitservice/Pos365RetrofitService.java
+++ src/main/java/com/jidian/cosalon/migration/pos365/retrofitservice/Pos365RetrofitService.java
@@ -2,24 +2,23 @@ package com.jidian.cosalon.migration.pos365.retrofitservice;
 
 import com.jidian.cosalon.migration.pos365.domainpos365.Pos365Branch;
 import com.jidian.cosalon.migration.pos365.domainpos365.Pos365Product;
+import com.jidian.cosalon.migration.pos365.domainpos365.Pos365ProductHistory;
 import com.jidian.cosalon.migration.pos365.domainpos365.Pos365Transfer;
+import com.jidian.cosalon.migration.pos365.domainpos365.Pos365User;
 import com.jidian.cosalon.migration.pos365.domainpos365.Post365Categories;
 import com.jidian.cosalon.migration.pos365.domainpos365.Post365Items;
-import com.jidian.cosalon.migration.pos365.domainpos365.Pos365ProductHistory;
-import com.jidian.cosalon.migration.pos365.domainpos365.Pos365User;
 import com.jidian.cosalon.migration.pos365.domainpos365.Post365OrderStock;
 import com.jidian.cosalon.migration.pos365.dto.BaseResponse;
 import com.jidian.cosalon.migration.pos365.dto.LoginRequest;
 import com.jidian.cosalon.migration.pos365.dto.LoginResponse;
-import java.util.Map;
-import java.util.concurrent.Executor;
-
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.HeaderMap;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
+
+import java.util.Map;
 
 public interface Pos365RetrofitService {
 
@@ -31,15 +30,15 @@ public interface Pos365RetrofitService {
 
     @GET("users?format=json")
     Call<BaseResponse<Pos365User>> listUsers(@HeaderMap Map<String, String> headers,
-        @Query("top") Integer top, @Query("skip") Integer skip);
+                                             @Query("top") Integer top, @Query("skip") Integer skip);
 
     @GET("products?Type=1&CategoryId=-1")
     Call<BaseResponse<Pos365Product>> listProducts(@HeaderMap Map<String, String> headers,
-        @Query("top") Integer top, @Query("skip") Integer skip);
+                                                   @Query("top") Integer top, @Query("skip") Integer skip);
 
     @GET("transfers")
     Call<BaseResponse<Pos365Transfer>> listTransfers(@HeaderMap Map<String, String> headers,
-        @Query("top") Integer top, @Query("skip") Integer skip);
+                                                     @Query("top") Integer top, @Query("skip") Integer skip);
 
     @GET("categories?format=json")
     Call<BaseResponse<Post365Categories>> listCategories(@HeaderMap Map<String, String> headers);
@@ -51,6 +50,6 @@ public interface Pos365RetrofitService {
     Call<BaseResponse<Pos365ProductHistory>> listProductsHistory(@HeaderMap Map<String, String> headers, @Query("top") Integer top, @Query("skip") Integer skip,
                                                                  @Query("ProductId") Long productId, @Query("BranchId") Long branchId);
 
-    @GET("orderstock?format=json")
-    Call<BaseResponse<Post365OrderStock>> listOrderStock(@HeaderMap Map<String, String> headers);
+    @GET("https://cosalon.pos365.vn/api/orderstock?")
+    Call<BaseResponse<Post365OrderStock>> listOrderStock(@HeaderMap Map<String, String> headers, @Query("top") Integer top, @Query("skip") Integer skip);
 }
