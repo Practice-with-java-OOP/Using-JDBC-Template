4c1a9ac0dc264ed9f9672048ec4afae00c1e7e84
Mai Tien Hai <maitienhai@velacorp.vn>
2019-10-15 16:39:24 +0700

    thread fetch returns

---
+++ src/main/java/com/jidian/cosalon/migration/pos365/domainpos365/Pos365Return.java
@@ -0,0 +1,58 @@
+package com.jidian.cosalon.migration.pos365.domainpos365;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "p365_return")
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Pos365Return {
+    @JsonProperty("Id")
+    @Id
+    private Long id;
+    @JsonProperty("Code")
+    private String code;
+    @JsonProperty("BranchId")
+    private Long branchId;
+    @JsonProperty("CreatedDate")
+    private String createdDate;
+    @JsonProperty("CreatedBy")
+    private Long createdBy;
+    @JsonProperty("Status")
+    private Long status;
+    @JsonProperty("Description")
+    private String description;
+    @JsonProperty("RetailerId")
+    private Long retailerId;
+    @JsonProperty("ReturnDate")
+    private String returnDate;
+    @JsonProperty("ModifiedDate")
+    private String modifiedDate;
+    @JsonProperty("ModifiedBy")
+    private Long modifiedBy;
+    @JsonProperty("Total")
+    private BigDecimal total;
+    @JsonProperty("Discount")
+    private BigDecimal discount;
+    @JsonProperty("TotalPayment")
+    private BigDecimal totalPayment;
+    @JsonProperty("AccountingTransactions")
+    @Transient
+    private List<Object> accountingTransactions;
+    @JsonProperty("ReturnDetails")
+    @Transient
+    private List<Object> returnDetails;
+}
---
+++ src/main/java/com/jidian/cosalon/migration/pos365/thread/impl/ReturnThread.java
@@ -0,0 +1,59 @@
+package com.jidian.cosalon.migration.pos365.thread.impl;
+
+import com.jidian.cosalon.migration.pos365.domainpos365.Pos365Product;
+import com.jidian.cosalon.migration.pos365.domainpos365.Pos365Return;
+import com.jidian.cosalon.migration.pos365.dto.BaseResponse;
+import com.jidian.cosalon.migration.pos365.thread.MyThread;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component("returnThread")
+public class ReturnThread extends MyThread {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReturnThread.class);
+
+    @Override
+    public String getName() {
+        return "ReturnThread";
+    }
+
+    @Override
+    public void doRun() {
+        int skip = 0;
+        int top = 100;
+        int count = 0;
+        int insertedTotal = 0;
+        int assumptionTotal = 0;
+
+        try {
+            jdbcTemplate.execute("TRUNCATE TABLE p365_return");
+            do {
+                BaseResponse<Pos365Return> response = pos365RetrofitService.listReturn(getMapHeaders2(), top, skip).execute().body();
+                if (response != null) {
+                    skip += top;
+                    assumptionTotal = response.getCount();
+                }
+                count = 0;
+                if (response != null && response.getResults() != null) {
+                    count = response.getResults().size();
+                    response.getResults().forEach(item -> {
+                        jdbcTemplate.update(
+                                "INSERT INTO p365_return " +
+                                        "    (id, branch_id, code, created_by, created_date, description, discount, modified_by, modified_date, " +
+                                        "    retailer_id, return_date, status, total, total_payment) " +
+                                        "    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+                                item.getId(), item.getBranchId(), item.getCode(), item.getCreatedBy(), item.getCreatedDate(), item.getDescription(), item.getDiscount(), item.getModifiedBy(), item.getModifiedDate(),
+                                item.getRetailerId(), item.getReturnDate(), item.getStatus(), item.getTotal(), item.getTotalPayment());
+                    });
+                    jdbcTemplate.execute("COMMIT");
+                    insertedTotal += response.getResults().size();
+                }
+            } while (count > 0);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        } finally {
+            LOGGER.info("SUMMARY: insertedTotal: {}, assumptionTotal: {}", insertedTotal, assumptionTotal);
+        }
+    }
+}
