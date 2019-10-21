b3c980a279749ec303373af4f2d157bb6ce168ac
Mai Tien Hai <maitienhai@velacorp.vn>
2019-10-21 16:31:20 +0700

    script migrate store tu DB prod, map giua store ma warehouse

---
+++ src/main/resources/db/migration/migrate_store.sql
@@ -0,0 +1,29 @@
+SET FOREIGN_KEY_CHECKS = 0;
+delete from cosalon_bhair.bhair_store;
+commit ;
+SET FOREIGN_KEY_CHECKS = 1;
+
+INSERT INTO cosalon_bhair.bhair_store
+(id, gmt_create, gmt_modified, version, address, avatar, brand_name, city_code, city_name, district_name, cumulative_order_quantity, cumulative_turnover, exhibition_image_urls, introduction, lat, lng, monthly_order_quantity, monthly_turnover, name, nursing_staff_quantity, status, stylist_quantity, telephone, type_code)
+VALUES(13, '2019-08-12 09:44:32.000', '2019-08-13 02:59:39.000', 3, '200 Nguyễn Tuân', '2019/08/09/e17ca0f7a55f43b2a4cf958aa819f263nguyen_tuan2.jpg', 'Cosalon', '', 'Hà nội', NULL, 0, 0.00, 'https://s3-ap-southeast-1.amazonaws.com/cosalon-static-staging/2019/06/27/5df6b91087f74f8aa5eec37aa0f76755nuyen tuan.jpg', '<p>Đ&acirc;y l&agrave; địa chỉ đầu ti&ecirc;n của m&ocirc; h&igrave;nh Coworking &ndash; CoSalon tại Việt Nam. V&agrave;&nbsp;l&agrave; sự kiện đ&aacute;nh dấu bước ngoặt quan trọng cho c&aacute;c stylist đang hoạt động trong ng&agrave;nh t&oacute;c c&oacute; nhu cầu l&agrave;m chủ salon của ri&ecirc;ng m&igrave;nh.</p>', 21.002, 105.801, 0, 0.00, 'Cosalon Nguyễn Tuân', 0, 1, 0, '0247 303 5533', 1);
+INSERT INTO cosalon_bhair.bhair_store
+(id, gmt_create, gmt_modified, version, address, avatar, brand_name, city_code, city_name, district_name, cumulative_order_quantity, cumulative_turnover, exhibition_image_urls, introduction, lat, lng, monthly_order_quantity, monthly_turnover, name, nursing_staff_quantity, status, stylist_quantity, telephone, type_code)
+VALUES(14, '2019-08-12 09:44:32.000', '2019-08-13 02:59:29.000', 2, '12 Vũ Phạm Hàm', '2019/06/27/3c2de8308a054cf99af778d27a7dcba1vu pham ham.jpg', 'Cosalon ', '', 'Hà Nội', NULL, 0, 0.00, 'https://s3-ap-southeast-1.amazonaws.com/cosalon-static-staging/2019/06/27/1e8b34f7fc54437d81c532ba2564a2advu pham ham.jpg', '<p>&nbsp;Co Salon cơ sở thứ 2 tại địa chỉ 12 Vũ Phạm H&agrave;m &ndash; Cầu Giấy &ndash; H&agrave; Nội. L&agrave; một &rdquo; đứa con&rdquo; nằm trong chuỗi hệ thống Co Salon, Co Salon 12 Vũ Phạm H&agrave;m được đặt nhiều kỳ vọng sẽ trở th&agrave;nh địa điểm l&agrave;m đẹp l&yacute; tưởng của kh&aacute;ch h&agrave;ng với ti&ecirc;u ch&iacute;: &ldquo;Y&ecirc;n t&acirc;m &ndash; An to&agrave;n &ndash; Kh&ocirc;ng chờ đợi&rdquo;.</p>', 21.015, 105.797, 0, 0.00, 'Cosalon Vũ Phạm Hàm', 0, 1, 0, '0247 303 5533', 1);
+INSERT INTO cosalon_bhair.bhair_store
+(id, gmt_create, gmt_modified, version, address, avatar, brand_name, city_code, city_name, district_name, cumulative_order_quantity, cumulative_turnover, exhibition_image_urls, introduction, lat, lng, monthly_order_quantity, monthly_turnover, name, nursing_staff_quantity, status, stylist_quantity, telephone, type_code)
+VALUES(15, '2019-08-12 08:15:34.000', '2019-08-13 02:59:20.000', 3, '121 Đội Cấn, Ba Đình', '2019/08/12/214d6bc1595d478785bce99862653da6COSALON Comingsoon.jpg', 'Cosalon', '', 'Ha Noi', NULL, 0, 0.00, 'https://s3-ap-southeast-1.amazonaws.com/cosalon-static/2019/08/12/241b02dfb26e4729b1e3546d0e37e7dfCOSALON Comingsoon.jpg', '<p>Coming soon</p>', 21.035, 105.828, 0, 0.00, '121 Đội Cấn, Ba Đình - Coming soon', 0, 1, 0, '0247 303 5533', 1);
+INSERT INTO cosalon_bhair.bhair_store
+(id, gmt_create, gmt_modified, version, address, avatar, brand_name, city_code, city_name, district_name, cumulative_order_quantity, cumulative_turnover, exhibition_image_urls, introduction, lat, lng, monthly_order_quantity, monthly_turnover, name, nursing_staff_quantity, status, stylist_quantity, telephone, type_code)
+VALUES(16, '2019-08-12 08:16:57.000', '2019-08-13 02:59:11.000', 3, '47 Nguyễn Du, Hai Bà Trưng', '2019/08/12/e85c98c76d31416a90901622fb1e400dCOSALON Comingsoon.jpg', 'Cosalon', '', 'Ha Noi', NULL, 0, 0.00, 'https://s3-ap-southeast-1.amazonaws.com/cosalon-static/2019/08/12/2e5b35fa69f14f8c99f656b588c3ab9eCOSALON Comingsoon.jpg', '<p>Coming soon</p>', 21.019, 105.85, 0, 0.00, '47 Nguyễn Du, Hai Bà Trưng - Coming soon', 0, 1, 0, '0247 303 5533', 1);
+INSERT INTO cosalon_bhair.bhair_store
+(id, gmt_create, gmt_modified, version, address, avatar, brand_name, city_code, city_name, district_name, cumulative_order_quantity, cumulative_turnover, exhibition_image_urls, introduction, lat, lng, monthly_order_quantity, monthly_turnover, name, nursing_staff_quantity, status, stylist_quantity, telephone, type_code)
+VALUES(17, '2019-08-12 08:17:55.000', '2019-10-19 07:08:35.000', 3, 'BT12, số 8,9 khu đô thị Xala, Hà Đông ', '2019/10/19/9e28193434194eafbb85509feb5b0cf3salon xala.jpg', 'Cosalon', '', 'Ha Noi', 'Hà Đông', 0, 0.00, '2019/10/19/f4539f3f90074b289b02b9e72c557571salon xala.jpg', '<p>Cosalon Xala</p>', 20.962, 105.792, 0, 0.00, 'Cosalon Xala', 0, 1, 0, '0247 303 5533', 1);
+commit ;
+
+truncate table cosalon_ims.ims_warehouse_store;
+
+INSERT INTO cosalon_ims.ims_warehouse_store (gmt_create, gmt_modified, version, warehouse_id, store_id, store_name) VALUES (CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP(),0,1,13,'Cosalon Nguyễn Tuân');
+INSERT INTO cosalon_ims.ims_warehouse_store (gmt_create, gmt_modified, version, warehouse_id, store_id, store_name) VALUES (CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP(),0,3,14,'Cosalon Vũ Phạm Hàm');
+INSERT INTO cosalon_ims.ims_warehouse_store (gmt_create, gmt_modified, version, warehouse_id, store_id, store_name) VALUES (CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP(),0,4,17,'Cosalon Xala');
+INSERT INTO cosalon_ims.ims_warehouse_store (gmt_create, gmt_modified, version, warehouse_id, store_id, store_name) VALUES (CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP(),0,5,15,'121 Đội Cấn, Ba Đình - Coming soon');
+commit ;
