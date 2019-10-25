a173fff8efc98c7a4a8e2f8d5df7bbf6bcc343a1
minhlee <lengocminh@unimarmores.com>
2019-10-25 18:39:32 +0700

    migrate stylist raw data from p365 to bhair_stylist

---
+++ src/main/java/com/jidian/cosalon/migration/pos365/thread/imsimpl/BhairStylistThread.java
@@ -0,0 +1,86 @@
+package com.jidian.cosalon.migration.pos365.thread.imsimpl;
+
+import com.jidian.cosalon.migration.pos365.Utils;
+import com.jidian.cosalon.migration.pos365.domainpos365.Pos365Partner;
+import com.jidian.cosalon.migration.pos365.thread.MyThread;
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+
+@Component("bhairStylistThread")
+public class BhairStylistThread extends MyThread {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BhairStylistThread.class);
+    private int insertedTotal = 0;
+    private int assumptionTotal = 0;
+
+    @Override
+    public String getName() {
+        return "BhairStylistThread";
+    }
+
+    @Override
+    public void doRun() {
+        try {
+            final List<Pos365Partner> stylists = jdbcTemplate.query("select * from p365_partners "
+                    + " where lower(name) like '%h01c%' or lower(name) like '%h02s%' or lower(name) like '%h03s0%'",
+                (rs, rowNum) -> {
+                    final Pos365Partner result = new Pos365Partner();
+                    result.setId(rs.getLong("id"));
+                    result.setBranchId(rs.getLong("branch_id"));
+                    result.setCode(rs.getString("code"));
+                    result.setCreatedBy(rs.getLong("created_by"));
+                    result.setCreatedDate(rs.getString("created_date"));
+                    result.setGender(rs.getInt("gender"));
+                    result.setModifiedDate(rs.getString("modified_date"));
+                    result.setName(rs.getString("name"));
+                    result.setPhone(rs.getString("phone"));
+                    result.setPoint(rs.getInt("point"));
+                    result.setTotalDebt(rs.getBigDecimal("total_debt"));
+                    return result;
+                });
+            assumptionTotal = stylists.size();
+            stylists.forEach(stylist -> {
+                KeyHolder keyHolder = new GeneratedKeyHolder();
+                insertedTotal += bhairJdbcTemplate.update(
+                    connection -> {
+                        PreparedStatement ps = connection.prepareStatement(
+                            "insert into bhair_stylist"
+                                + " (gmt_create, gmt_modified, version, attitude_score, "
+                                + " available_item_ids, available_item_names, avatar, "
+                                + " comment_quantity, cumulative_order_quantity, cumulative_turnover, "
+                                + " display_name, email, experience_score, favorites_quantity, "
+                                + " introduction, level_id, level_name, monthly_order_quantity, "
+                                + " monthly_turnover, overall_score, password, phone_num, "
+                                + " rated_quantity, signature, skill_score, slogan, status, type, "
+                                + " user_name, working_years, store_id, highlight, weights)"
+                                + " values (?, ?, 0, 0, null, null, null, 0, 0, 0, ?, null, 0, 0, "
+                                + " null, 0, 'p365', 0, 0, 0, null, ?, 0, null, 0, null, 9, 1,"
+                                + " ?, 0, (select s.id from bhair_store s where s.name = 'POS365 Branch'), 0, 100);",
+                            new String[]{"id"});
+                        int index = 1;
+                        ps.setTimestamp(index++, Utils.convertTimestamp(stylist.getCreatedDate()));
+                        ps.setTimestamp(index++, new Timestamp(System.currentTimeMillis()));
+                        ps.setString(index++, stylist.getName());
+                        ps.setString(index++, stylist.getPhone() == null ?
+                            Utils.genP365PhoneNumber(stylist.getId().toString()) : Utils
+                            .genP365PhoneNumber(stylist.getPhone()));
+                        ps.setString(index, stylist.getCode());
+                        return ps;
+                    },
+                    keyHolder
+                );
+            });
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        } finally {
+            LOGGER.info("SUMMARY: insert/update total: {}, Pos365 Stylist total: {}", insertedTotal,
+                assumptionTotal);
+        }
+    }
+}
